e951307c8b17a2ab9f2832d8e7679ff1b06178e1
Guillaume Brunerie <guillaume.brunerie@elk-studios.com>
2025-04-07 12:31:53 +0200

    tweaks

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -863,74 +863,62 @@ there should still be identified correctly.
  '(TeX-command-BibTeX "Biber")
  '(TeX-command-list
    '(("TeX" "%(PDF)%(tex) %`%S%(PDFout)%(mode)%' %t" TeX-run-TeX nil
-      (plain-tex-mode texinfo-mode ams-tex-mode)
-      :help "Run plain TeX")
-     ("LaTeX" "%`%l%(mode)%' %t" TeX-run-TeX nil
-      (latex-mode doctex-mode)
-      :help "Run LaTeX")
-     ("Makeinfo" "makeinfo %t" TeX-run-compile nil
-      (texinfo-mode)
-      :help "Run Makeinfo with Info output")
-     ("Makeinfo HTML" "makeinfo --html %t" TeX-run-compile nil
-      (texinfo-mode)
+      (plain-tex-mode texinfo-mode ams-tex-mode) :help "Run plain TeX")
+     ("LaTeX" "%`%l%(mode)%' %t" TeX-run-TeX nil (latex-mode doctex-mode) :help
+      "Run LaTeX")
+     ("Makeinfo" "makeinfo %t" TeX-run-compile nil (texinfo-mode) :help
+      "Run Makeinfo with Info output")
+     ("Makeinfo HTML" "makeinfo --html %t" TeX-run-compile nil (texinfo-mode)
       :help "Run Makeinfo with HTML output")
      ("AmSTeX" "%(PDF)amstex %`%S%(PDFout)%(mode)%' %t" TeX-run-TeX nil
-      (ams-tex-mode)
-      :help "Run AMSTeX")
+      (ams-tex-mode) :help "Run AMSTeX")
      ("ConTeXt" "texexec --once --texutil %(execopts)%t" TeX-run-TeX nil
-      (context-mode)
-      :help "Run ConTeXt once")
-     ("ConTeXt Full" "texexec %(execopts)%t" TeX-run-TeX nil
-      (context-mode)
+      (context-mode) :help "Run ConTeXt once")
+     ("ConTeXt Full" "texexec %(execopts)%t" TeX-run-TeX nil (context-mode)
       :help "Run ConTeXt until completion")
      ("BibTeX" "bibtex %s" TeX-run-BibTeX nil t :help "Run BibTeX")
      ("View" "evince %s.pdf" TeX-run-discard t t :help "Run Viewer")
      ("Print" "%p" TeX-run-command t t :help "Print the file")
-     ("Queue" "%q" TeX-run-background nil t :help "View the printer queue" :visible TeX-queue-command)
-     ("File" "%(o?)dvips %d -o %f " TeX-run-command t t :help "Generate PostScript file")
+     ("Queue" "%q" TeX-run-background nil t :help "View the printer queue"
+      :visible TeX-queue-command)
+     ("File" "%(o?)dvips %d -o %f " TeX-run-command t t :help
+      "Generate PostScript file")
      ("Index" "makeindex %s" TeX-run-command nil t :help "Create index file")
-     ("Check" "lacheck %s" TeX-run-compile nil
-      (latex-mode)
-      :help "Check LaTeX file for correctness")
-     ("Spell" "(TeX-ispell-document \"\")" TeX-run-function nil t :help "Spell-check the document")
-     ("Clean" "TeX-clean" TeX-run-function nil t :help "Delete generated intermediate files")
-     ("Clean All" "(TeX-clean t)" TeX-run-function nil t :help "Delete generated intermediate and output files")
+     ("Check" "lacheck %s" TeX-run-compile nil (latex-mode) :help
+      "Check LaTeX file for correctness")
+     ("Spell" "(TeX-ispell-document \"\")" TeX-run-function nil t :help
+      "Spell-check the document")
+     ("Clean" "TeX-clean" TeX-run-function nil t :help
+      "Delete generated intermediate files")
+     ("Clean All" "(TeX-clean t)" TeX-run-function nil t :help
+      "Delete generated intermediate and output files")
      ("Other" "" TeX-run-command t t :help "Run an arbitrary command")
      ("Biber" "biber %s.bcf" TeX-run-Biber nil t)))
  '(TeX-electric-sub-and-superscript nil)
  '(TeX-fold-macro-spec-list
-   '(("[f]"
-      ("footnote"))
-     ("[i]"
-      ("index"))
-     ("*"
-      ("item"))
-     ("..."
-      ("dots"))
+   '(("[f]" ("footnote")) ("[i]" ("index")) ("*" ("item")) ("..." ("dots"))
      (1
-      ("emph" "textit" "textsl" "textmd" "textrm" "textsf" "texttt" "textbf" "textsc" "textup"))))
+      ("emph" "textit" "textsl" "textmd" "textrm" "textsf" "texttt" "textbf"
+       "textsc" "textup"))))
  '(TeX-fold-type-list '(env macro comment))
  '(TeX-newline-function 'reindent-then-newline-and-indent)
  '(TeX-open-quote "\"")
  '(TeX-output-view-style
-   '(("^dvi$"
-      ("^landscape$" "^pstricks$\\|^pst-\\|^psfrag$")
+   '(("^dvi$" ("^landscape$" "^pstricks$\\|^pst-\\|^psfrag$")
       "%(o?)dvips -t landscape %d -o && gv %f")
      ("^dvi$" "^pstricks$\\|^pst-\\|^psfrag$" "%(o?)dvips %d -o && gv %f")
-     ("^dvi$"
-      ("^a4\\(?:dutch\\|paper\\|wide\\)\\|sem-a4$" "^landscape$")
+     ("^dvi$" ("^a4\\(?:dutch\\|paper\\|wide\\)\\|sem-a4$" "^landscape$")
       "%(o?)xdvi %dS -paper a4r -s 0 %d")
-     ("^dvi$" "^a4\\(?:dutch\\|paper\\|wide\\)\\|sem-a4$" "%(o?)xdvi %dS -paper a4 %d")
-     ("^dvi$"
-      ("^a5\\(?:comb\\|paper\\)$" "^landscape$")
+     ("^dvi$" "^a4\\(?:dutch\\|paper\\|wide\\)\\|sem-a4$"
+      "%(o?)xdvi %dS -paper a4 %d")
+     ("^dvi$" ("^a5\\(?:comb\\|paper\\)$" "^landscape$")
       "%(o?)xdvi %dS -paper a5r -s 0 %d")
      ("^dvi$" "^a5\\(?:comb\\|paper\\)$" "%(o?)xdvi %dS -paper a5 %d")
      ("^dvi$" "^b5paper$" "%(o?)xdvi %dS -paper b5 %d")
      ("^dvi$" "^letterpaper$" "%(o?)xdvi %dS -paper us %d")
      ("^dvi$" "^legalpaper$" "%(o?)xdvi %dS -paper legal %d")
      ("^dvi$" "^executivepaper$" "%(o?)xdvi %dS -paper 7.25x10.5in %d")
-     ("^dvi$" "." "%(o?)xdvi %dS %d")
-     ("^pdf$" "." "evince %o")
+     ("^dvi$" "." "%(o?)xdvi %dS %d") ("^pdf$" "." "evince %o")
      ("^html?$" "." "netscape %o")))
  '(agda2-highlight-level 'interactive)
  '(agda2-include-dirs '("." ".."))
@@ -942,6 +930,7 @@ there should still be identified correctly.
  '(current-language-environment "UTF-8")
  '(custom-safe-themes
    '("78e9a3e1c519656654044aeb25acb8bec02579508c145b6db158d2cfad87c44e" default))
+ '(dired-movement-style 'bounded)
  '(fill-column 80)
  '(haskell-mode-hook '(turn-on-haskell-indent))
  '(home-end-enable t)
@@ -953,8 +942,7 @@ there should still be identified correctly.
  '(outline-regexp "[\12]*" t)
  '(preview-auto-cache-preamble t)
  '(preview-default-preamble
-   '("\\RequirePackage["
-     ("," . preview-default-option-list)
+   '("\\RequirePackage[" ("," . preview-default-option-list)
      "]{preview}[2004/11/05]"))
  '(preview-preserve-counters t)
  '(preview-scale-function 1.3)
@@ -965,29 +953,16 @@ there should still be identified correctly.
            ((default-directory
              (locate-dominating-file buffer-file-name ".dir-locals.el")))
            (make-local-variable 'coq-prog-name)
-           (setq coq-prog-name
-                 (expand-file-name "../hoqtop")))))
+           (setq coq-prog-name (expand-file-name "../hoqtop")))))
  '(sentence-end-double-space nil)
  '(tab-width 4)
  '(tree-sitter-debug-highlight-jump-region t)
  '(tree-sitter-debug-jump-buttons t)
  '(vc-annotate-color-map
-   '((20 . "#FA99B7")
-     (40 . "#FE9A9F")
-     (60 . "#F89F88")
-     (80 . "#ECA777")
-     (100 . "#D9AF6D")
-     (120 . "#C2B86D")
-     (140 . "#A8BF75")
-     (160 . "#8BC485")
-     (180 . "#6CC89C")
-     (200 . "#49C9B5")
-     (220 . "#21C9CE")
-     (240 . "#0EC7E4")
-     (260 . "#3CC3F5")
-     (280 . "#6ABDFE")
-     (300 . "#95B5FE")
-     (320 . "#BAACF6")))
+   '((20 . "#FA99B7") (40 . "#FE9A9F") (60 . "#F89F88") (80 . "#ECA777")
+     (100 . "#D9AF6D") (120 . "#C2B86D") (140 . "#A8BF75") (160 . "#8BC485")
+     (180 . "#6CC89C") (200 . "#49C9B5") (220 . "#21C9CE") (240 . "#0EC7E4")
+     (260 . "#3CC3F5") (280 . "#6ABDFE") (300 . "#95B5FE") (320 . "#BAACF6")))
  '(warning-suppress-log-types '((comp) (lsp-mode) (lsp-mode)))
  '(warning-suppress-types '((lsp-mode) (lsp-mode)))
  '(whitespace-style '(face trailing lines-tail empty tabs)))
